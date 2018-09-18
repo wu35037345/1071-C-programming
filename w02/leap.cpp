24e87f184f8abc0b07051fe86d08d34a40d5d7f8
wu35037345 <43335083+wu35037345@users.noreply.github.com>
2018-09-18 16:26:43 +0800

    Add leap.cpp

---
+++ w02/leap.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+main()
+{
+	int year;
+	printf("Enter year:");
+	scanf("%d",&year);
+	
+	if((year%4 ==0 && !(year%100==0))||year%400==0){
+		printf("Year %d is a leap year",year);
+	}else{
+		printf("Year %d is not a leap year",year);
+	}
+	return 0;
+}

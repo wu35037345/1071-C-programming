c75daef0967a43b65562ade0315038a0a3a08045
wu35037345 <43335083+wu35037345@users.noreply.github.com>
2018-09-18 16:44:49 +0800

    Add sec.cpp

---
+++ w02/sec.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+main(){
+	int sec;
+	int hh,mm,ss;
+	printf("Enter sec:");
+	scanf("%d",&sec);
+	hh = (sec/3600);
+	mm = (sec%3600)/60;
+	ss = (sec%60);
+	printf("%d sec=%02d:%02d:%02d\n",sec,hh,mm,ss);
+	
+	return 0;
+}
+
